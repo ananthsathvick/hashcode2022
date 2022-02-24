893bc450a231cf478985de6d63486e81a2dac221
ananth sathvick <ananthsathvick@gmail.com>
2022-02-24 23:11:08 +0530

    Initialize

---
+++ A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+typedef long long ll;
+#define fio                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    fio;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    // t =1;
+    for (ll tt = 0; tt < t; tt++)
+    {
+        // cout<<"Case #"<<tt+1<<": ";
+        ll n;
+        cin >> n ;
+        vector<ll> a(n);
+        for (ll i = 0; i < n; i++)
+        {
+           cin>>a[i];
+        }
+
+    }
+    return 0;
+}
+/*
+ */
